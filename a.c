825a7c47c32ec5f414f6837d0be7c5fdbbd165c3
sanchit324 <me2211063@iitd.ac.in>
2025-03-03 22:00:16 +0530

    sample commit

--- a.c
+++ a.c
@@ -1,21 +1,22 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]){
-	int first = atoi(argv[1]);
-	int second = atoi(argv[3]);
-	char sign = argv[2][0];
-	int res = 0;
-	if(sign == '+'){
-		res = first+second;
-	}else if(sign == '-'){
-		res = first-second;
-	}else if(sign == '*'){
-		res = first*second;
-	}else if(sign == '/'){
-		res = first/second;
-	}
-	printf("%d\n",res);
-	return 0;
+    int first;
+    int second;
+    char sign;
+    int res = 0;
+    scanf("%d %c %d", &first, &sign, &second);
+    if(sign == '+'){
+        res = first+second;
+    }else if(sign == '-'){
+        res = first-second;
+    }else if(sign == '*'){
+        res = first*second;
+    }else if(sign == '/'){
+        res = first/second;
+    }
+    printf("%d", res);
+    return 0;
 }
